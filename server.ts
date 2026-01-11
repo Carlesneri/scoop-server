8bb86b59c73fe633c5c7fc414de28f140d5e40db
carlesneri <carlesneri@hotmail.com>
2026-01-11 21:01:12 +0100

    Create server

---
+++ server.ts
@@ -0,0 +1,19 @@
+import { serve } from "@hono/node-server"
+import { serveStatic } from "@hono/node-server/serve-static"
+import { Hono } from "hono"
+
+const app = new Hono()
+
+app.use("*", serveStatic({ root: "./static" }))
+
+app.get("/", (c) => c.text("Hello Node.js!"))
+
+serve(
+	{
+		fetch: app.fetch,
+		port: 0,
+	},
+	async ({ port }) => {
+		console.log("Serving in port: ", port)
+	},
+)
